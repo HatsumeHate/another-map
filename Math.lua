7ff0101db90644228bd11e45feba4c89d5d67749
nazarpunk <nazarpunk@ya.ru>
2019-11-15 18:02:54 +0200

    Add Math.lua

---
+++ Math.lua
@@ -0,0 +1,140 @@
+---@param x real
+---@param y real
+---@return boolean
+function InMapXY(x, y)
+	return x > GetRectMinX(bj_mapInitialPlayableArea) and x < GetRectMaxX(bj_mapInitialPlayableArea) and y > GetRectMinY(bj_mapInitialPlayableArea) and y < GetRectMaxY(bj_mapInitialPlayableArea)
+end
+
+---@param x real
+---@param distance real
+---@param angle real radian
+---@return real
+function GetPolarOffsetX(x, distance, angle)
+	return distance * math.cos(angle) + x
+end
+
+---@param y real
+---@param distance real
+---@param angle real radian
+---@return real
+function GetPolarOffsetY(y, distance, angle)
+	return distance * math.sin(angle) + y
+end
+
+---@param x real
+---@param y real
+---@param distance real
+---@param angle real radian
+---@return real, real
+function GetPolarOffsetXY(x, y, distance, angle)
+	return distance * math.cos(angle) + x, distance * math.sin(angle) + y
+end
+
+local GetTerrainZ_location = Location(0, 0)
+---@param x real
+---@param y real
+---@return real
+function GetTerrainZ(x, y)
+	MoveLocation(GetTerrainZ_location, x, y)
+	return GetLocationZ(GetTerrainZ_location)
+end
+
+---@param target unit
+---@return real
+function GetUnitZ(target)
+	MoveLocation(GetTerrainZ_location, GetUnitX(target), GetUnitY(target))
+	return GetLocationZ(GetTerrainZ_location) + GetUnitFlyHeight(target)
+end
+
+local SetUnitZ_ability = FourCC('Aave')
+---@param target unit
+---@param z real
+function SetUnitZ(target, z)
+	UnitAddAbility(target, SetUnitZ_ability)
+	UnitRemoveAbility(target, SetUnitZ_ability)
+	MoveLocation(GetTerrainZ_location, GetUnitX(target), GetUnitY(target))
+	SetUnitFlyHeight(target, z - GetLocationZ(GetTerrainZ_location), 0)
+end
+
+---@param h real максимальная высота в прыжке на середине расстояния (x = d / 2)
+---@param d real общее расстояние до цели
+---@param x real расстояние от исходной цели до точки, где следует взять высоту по параболе
+---@return real
+function ParabolaZ (h, d, x)
+	return (4 * h / d) * (d - x) * (x / d)
+end
+
+---@param zs real начальная высота одного края дуги
+---@param ze real конечная высота другого края дуги
+---@param h real максимальная высота на середине расстояния (x = d / 2)
+---@param d real общее расстояние до цели
+---@param x real расстояние от исходной цели до точки
+---@return real
+function GetParabolaZ(zs, ze, h, d, x)
+	return (2 * (zs + ze - 2 * h) * (x / d - 1) + ze - zs) * (x / d) + zs
+end
+
+---@param xa real
+---@param ya real
+---@param xb real
+---@param yb real
+---@return real
+function DistanceBetweenXY(xa, ya, xb, yb)
+	local dx, dy = xb - xa, yb - ya
+	return math.sqrt(dx * dx + dy * dy)
+end
+
+---@param xa real
+---@param ya real
+---@param za real
+---@param xb real
+---@param yb real
+---@param zb real
+---@return real
+function DistanceBetweenXYZ(xa, ya, za, xb, yb, zb)
+	local dx, dy, dz = xb - xa, yb - ya, zb - za
+	return math.sqrt(dx * dx + dy * dy + dz * dz)
+end
+
+---@param xa real
+---@param ya real
+---@param xb real
+---@param yb real
+---@return real radian
+function AngleBetweenXY(xa, ya, xb, yb)
+	return math.atan(yb - ya, xb - xa)
+end
+
+---@param a real radian
+---@param b real radian
+---@return real radian
+function AngleDifference(a, b)
+	local c = a > b and a - b or b - a
+	local d = a > b and b - a + 2 * math.pi or a - b + 2 * math.pi
+	return c > d and d or c
+end
+
+-- xgm.guru/p/wc3/warden-math
+---@param a real degrees
+---@param b real degrees
+---@return real degrees
+function AngleDifferenceDeg(a, b)
+	a, b = math.abs(a), math.abs(b)
+	if a > b then a, b = b, a end
+	local x = b - 360
+	if b - a > a - x then b = x end
+	return math.abs(a - b)
+end
+
+-- Находит длину перпендикуляра от отрезка, заданного xa, ya, xb, yb к точке, заданной xc, yc
+-- https://xgm.guru/p/wc3/perpendicular
+---@param xa real
+---@param ya real
+---@param xb real
+---@param yb real
+---@param xc real
+---@param yc real
+---@return real
+function Perpendicular (xa, ya, xb, yb, xc, yc)
+	return math.sqrt((xa - xc) * (xa - xc) + (ya - yc) * (ya - yc)) * math.sin(math.atan(yc - ya, xc - xa) - math.atan(yb - ya, xb - xa))
+end
