f083e47d250f726481416f453976afc8fb7974c0
nazarpunk <nazarpunk@ya.ru>
2019-11-19 14:51:10 +0200

    debug

--- PostInitLoader.lua
+++ PostInitLoader.lua
@@ -7,33 +7,55 @@ do
 		DefineSkillsData()
 		DefineEffectsData()
 		DefineParametersData()
-
-
-		BJDebugMsg("ok")
-
-		BJDebugMsg("=================")
-		BJDebugMsg("ЭФФЕКТЫ")
-		BJDebugMsg(EffectsData[1].name)
-		BJDebugMsg(EffectsData[1].level[1].Power)
-		BJDebugMsg(GetParameterName(EffectsData[1].level[1].Attribute))
-		BJDebugMsg("=================")
-		BJDebugMsg("ПРЕДМЕТЫ")
-		BJDebugMsg(ITEM_TEMPLATE_DATA[FourCC('I000')].NAME)
-		if ITEM_TEMPLATE_DATA[FourCC('I000')].BONUS[1] == nil then
-			BJDebugMsg("пустой бонус")
+		
+		print('Проверяем эффекты')
+		print('=================')
+		local raw  = 'I000'
+		local DATA = ITEM_TEMPLATE_DATA[FourCC(raw)]
+		if DATA == nil then
+			print('ОШИБКА: ', raw)
+			return
+		else
+			print('РАБОТАЕТ: ', raw)
+			print('=================')
 		end
-
-		BJDebugMsg(GetParameterName(ITEM_TEMPLATE_DATA[FourCC('I000')].BONUS[1].PARAMETER))
-		BJDebugMsg(ITEM_TEMPLATE_DATA[FourCC('I000')].BONUS[1].VALUE)
-		BJDebugMsg(ITEM_TEMPLATE_DATA[FourCC('I000')].BONUS[1].METHOD)
-
-		BJDebugMsg(GetParameterName(ITEM_TEMPLATE_DATA[FourCC('I000')].BONUS[2].PARAMETER))
-		BJDebugMsg(ITEM_TEMPLATE_DATA[FourCC('I000')].BONUS[2].VALUE)
-		BJDebugMsg(ITEM_TEMPLATE_DATA[FourCC('I000')].BONUS[2].METHOD)
-
-		BJDebugMsg("=================")
+		
+		for k, v in pairs(DATA) do
+			print(k, v)
+		end
+		print('=================')
+		print('БОНУСОВ', #DATA.BONUS)
+		if #DATA.BONUS > 0 then
+			for k, v in pairs(DATA.BONUS) do
+				for name, value in pairs(v) do
+					print(name, value)
+				end
+			end
+		end
+		
+		print('=================')
+		print("ЭФФЕКТЫ")
+		print(EffectsData[1].name)
+		print(EffectsData[1].level[1].Power)
+		print(GetParameterName(EffectsData[1].level[1].Attribute))
+		print('=================')
+		print("ПРЕДМЕТЫ")
+		print(ITEM_TEMPLATE_DATA[FourCC('I000')].NAME)
+		if ITEM_TEMPLATE_DATA[FourCC('I000')].BONUS[1] == nil then
+			print("пустой бонус")
+		end
+		
+		print(GetParameterName(ITEM_TEMPLATE_DATA[FourCC('I000')].BONUS[1].PARAMETER))
+		print(ITEM_TEMPLATE_DATA[FourCC('I000')].BONUS[1].VALUE)
+		print(ITEM_TEMPLATE_DATA[FourCC('I000')].BONUS[1].METHOD)
+		
+		print(GetParameterName(ITEM_TEMPLATE_DATA[FourCC('I000')].BONUS[2].PARAMETER))
+		print(ITEM_TEMPLATE_DATA[FourCC('I000')].BONUS[2].VALUE)
+		print(ITEM_TEMPLATE_DATA[FourCC('I000')].BONUS[2].METHOD)
+		
+		print('=================')
 		local my_item = CreateCustomItem('I000', GetRectCenterX(gg_rct_test), GetRectCenterY(gg_rct_test))
-
-		BJDebugMsg(ITEM_DATA[GetHandleId(my_item)].NAME)
+		
+		print(ITEM_DATA[GetHandleId(my_item)].NAME)
 	end
 end
